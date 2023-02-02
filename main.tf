37523aa5ad13c9507332c23140394711c96f72b9
Mahesh6111 <66183710+Mahesh6111@users.noreply.github.com>
2023-02-02 23:10:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "tfstorageaccountrg"
+    storage_account_name = "mytfstoragewb1"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "WBTestRg" {
+  name     = "MyWBRG"
+  location = "West Europe"
+}
